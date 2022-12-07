a36e4b3a5c064c2d538525daf656d71e01427745
Spencer Gerritsen <spencergerritsen@gmail.com>
2022-12-07 00:05:31 -0700

    webpack_plugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,7 +11,8 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      editor: './src/js/editor.js'
     },
     output: {
       filename: '[name].bundle.js',
