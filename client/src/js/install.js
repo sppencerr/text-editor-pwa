383cad7aa224081b7c1bcd894a770c6c87c4a053
Spencer Gerritsen <spencergerritsen@gmail.com>
2022-12-06 23:58:43 -0700

    install.2

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -9,7 +9,14 @@ window.addEventListener('beforeinstallprompt', (event) => {
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+
+    if(!promptEvent) {
+        return;
+    }
+    promptEvent.prompt();
+});
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {});
