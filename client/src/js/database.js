712642bd63f507a356601c105c859edaf44763f9
Spencer Gerritsen <spencergerritsen@gmail.com>
2022-12-13 01:01:40 -0700

    js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,29 +12,44 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Update method that accepts some content and updates it to the database
 export const putDb = async (content) => {
-  console.log('database put');
+  console.log('Put to the database');
 
-  const jateDb = await openDB('jate', 'readwrite');
+  // Create a connection to the database database and version we want to use.
+  const jateDb = await openDB('jate', 1);
 
-  const tx = jateDb.transaction('jate', 1);
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction('jate', 'readwrite');
 
+  // Open up the desired object store.
   const store = tx.objectStore('jate');
 
+  // Use the .put() method on the store and pass in the content.
   const request = store.put({ content: content });
 
+  // Get confirmation of the request.
   const result = await request;
-  console.log('database saved new', result);
+  console.log('🚀 - data saved to the database', result);
 };
 
+// Get method that gets all the content from the database
 export const getDb = async () => {
-  console.log('get from database');
+  console.log('GET from the database');
 
-  const jateDb = await openDB('jate');
+  // Create a connection to the database database and version we want to use.
+  const jateDb = await openDB('jate', 1);
 
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .getAll() method to get all data in the database.
   const request = store.getAll();
 
+  // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
   return result;
